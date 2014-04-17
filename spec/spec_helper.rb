1e722c09fae64b262853f2e7d802eedb1cab85c3
Juan Lulkin <maiz@lulk.in>
2014-04-17 23:11:04 +0300

    remove vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,16 +3,4 @@ $:.unshift File.expand_path("../../lib/bank_crawlers", __FILE__)
 require 'bundler/setup'
 require 'hpricot'
 require 'timecop'
-require 'vcr'
 require 'hapoalim'
-require 'webmock'
-
-VCR.configure do |config|
-  config.cassette_library_dir = 'spec/vcr_cassettes'
-  config.hook_into :webmock
-  config.allow_http_connections_when_no_cassette = false
-end
-
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-end
